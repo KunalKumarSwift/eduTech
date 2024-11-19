fb56a955f800fa8c0c766cbc6a597df9761cf801
Kunal Kumar <kunaldhiman91@gmail.com>
2024-11-18 19:53:09 -0500

    Fixed build issue

--- src/app/layout/layout.tsx
+++ src/app/layout/layout.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+//import { useState } from "react";
 import Sidebar from "@/components/Sidebar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
